bc334ebb0733ac4ecfea45be2362c595d8a53a9a
marcus <marquinhosq487@gmail.com>
2024-10-09 17:35:53 -0300

    Meu primeiro commit

--- main.py
+++ main.py
@@ -11,8 +11,10 @@ def multiplicar(num1 ,num2):
     return multiplicaçao
 
 def dividir(num1 , num2):
+    if num2 == 0 or num1 == 0:
+        return "Erro: Divisao por zero"
     divisao = num1 / num2
-    return divisao
+    return round(divisao, 1)
 
 operaçao = input("digite a operaçao soma, multiplicaçao, divisao ou subtraçao: ")
 num1 = int(input("Digite o primeiro numero: "))
@@ -21,19 +23,19 @@ num2 = int(input("Digite o segundo numero: "))
 
 if operaçao.lower() == "soma":
     soma = somar(num1 , num2)
-    print("a soma é:", soma)
+    print(f"A soma de {num1} e {num2} é igual a {soma}")
 
 elif operaçao.lower() == "subtraçao":
     subtraçao = subtrair(num1 , num2)
-    print("subtraçao é:",subtraçao)
+    print(f"A subtraçao de {num1} e {num2} é igual a {subtraçao}")
 
 elif operaçao.lower() == "multiplicaçao":
     multiplicaçao = multiplicar(num1 , num2)
-    print("a multiplicaçao é:", multiplicaçao)
+    print(f"A multiplicaçao de {num1} e {num2} é igual a {multiplicaçao}")
 
 elif operaçao.lower() == "divisao":
     divisao = dividir(num1 , num2)
-    print("a divisao é:",(divisao))
+    print(f"A divisao de {num1} e {num2} é igual a {divisao}")
     
 else:
     print("Digite uma operaçao valida")
